ac9a0cf56c269ebc0e87494b633906a2882fcf4e
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-09-23 17:55:50 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,38 +1,18 @@
-// import Player from '@vimeo/player';
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
-
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
 
-const player = new Player(document.getElementById('vimeo-player'));
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const _ = require('lodash');
 
-player.on('timeupdate', throttle(updateLocalTime, 1000));
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data);
+  }, 1000)
+);
 
-function updateLocalTime(event) {
-  const currentTime = event.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
-}
-
-const storedTime = localStorage.getItem('videoplayer-current-time');
-
-if (storedTime !== null) {
-  player.setCurrentTime(Number(storedTime));
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+  player.setCurrentTime(savedTime);
 }
