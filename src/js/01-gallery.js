5a761de752f60083cf69058aeb86e828e43f0d6b
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-09-23 16:56:09 +0300

    create vimeo/player, lodash; up task 02 (test);

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,8 +2,8 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css"
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
@@ -11,13 +11,25 @@ const gallery = document.querySelector('.gallery');
 const galleryItemMarkup = createGalleryItemMarkup(galleryItems);
 gallery.insertAdjacentHTML('beforeend', galleryItemMarkup);
 
-function createGalleryItemMarkup(galleryItems){
-    return galleryItems.map(({preview, original, description}) =>{
-        return ` <li class="gallery__item">
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
         <a class="gallery__link" href="${original}">
            <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
-     </li>`
-    }).join('');
-};
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 })
+     </li>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// У першому рядку коду ми імпортуємо galleryItems з файлу gallery-items.js, в якому знаходяться дані про нашу галерею.
+// Далі, ми імпортуємо плагін SimpleLightbox з пакету simplelightbox і його стилі з simple-lightbox.min.css.
+// Потім ми виводимо масив galleryItems у консоль, щоб перевірити, що дані були успішно імпортовані.
+// Далі, ми використовуємо document.querySelector('.gallery') для вибору елементу з класом gallery на нашій сторінці.
+// Наступна стрічка коду використовуємо createGalleryItemMarkup для генерації HTML розмітки, використовуючи дані з galleryItems.
+// Функція createGalleryItemMarkup отримує масив galleryItems як аргумент і використовує функцію map для створення розмітки для кожного елементу масиву. Кожен елемент масиву представлено об'єктом з властивостями preview, original і description.
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+// import Player from '@vimeo/player';
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
 
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const player = new Player(document.getElementById('vimeo-player'));
+
+player.on('timeupdate', throttle(updateLocalTime, 1000));
+
+function updateLocalTime(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+if (storedTime !== null) {
+  player.setCurrentTime(Number(storedTime));
+}
