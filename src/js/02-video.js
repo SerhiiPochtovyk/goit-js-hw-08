d31748de729100dfed06cc8929c51e78ca7eb442
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-09-25 11:18:06 +0300

    up task 2, 3;

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,3 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
-// const _ = require('lodash');
-
-// player.on(
-//   'timeupdate',
-//   _.throttle(data => {
-//     localStorage.setItem('videoplayer-current-time', data.seconds);
-//     console.log(data);
-//   }, 1000)
-// );
-
-// const savedTime = localStorage.getItem('videoplayer-current-time');
-// if (savedTime !== null) {
-//   player.setCurrentTime(savedTime);
-// }
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
